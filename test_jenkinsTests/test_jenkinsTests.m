d075ea2f4c09275ba75bb5fc765b5f18c24a658f
yassine benabbas <yassine.benabbas@gmail.com>
2016-05-31 22:30:57 +0200

    all test ok

--- test_jenkinsTests/test_jenkinsTests.m
+++ test_jenkinsTests/test_jenkinsTests.m
@@ -33,13 +33,14 @@
 - (void)testExampleFail {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssertTrue(NO);
+    XCTAssertTrue(YES);
 }
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+        XCTAssertTrue(YES);
     }];
 }
 
